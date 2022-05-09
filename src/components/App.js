5c6ddc47aad86d64319695235fb6ac631b22ef31
vasaman <amanvas2601@gmail.com>
2022-05-09 15:22:10 +0530

    4 files modified, 4 added

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import SignUp from './SignUp';
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
 
 export default function App() {
   return (
@@ -10,9 +15,17 @@ export default function App() {
       style={{ minHeight: '100vh' }}
     >
       <div className="w-100" style={{ maxWidth: '500px' }}>
-        <AuthProvider>
-          <SignUp />
-        </AuthProvider>
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route  path="/" element={} />
+            </Switch>
+            {/* <SignUp />
+            <Dashboard />
+            <Login />
+            <ForgotPassword /> */}
+          </AuthProvider>
+        </Router>
       </div>
     </Container>
   );
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -11,9 +11,22 @@ export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState();
   const [loading, setLoading] = useState(true);
 
-  function signUp(email, password) {
+  function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
+  function logout() {
+    return auth.signOut();
+  }
+
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -25,7 +38,10 @@ export function AuthProvider({ children }) {
 
   const value = {
     currentUser,
-    signUp,
+    login,
+    signup,
+    logout,
+    resetPassword,
   };
 
   return (
